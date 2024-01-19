c65895be44c05d1aec158a856718274d67046fcd
Bald Eagle <tsereteli.t02@gmail.com>
2024-01-19 16:18:51 +0400

    adding some logic for details movement

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,20 @@ import "./App.css";
 import sunIcon from "../assets/desktop/icon-sun.svg";
 import arrowUp from "../assets/desktop/icon-arrow-up.svg";
 import iconRefresh from "../assets/desktop/icon-refresh.svg";
+import { useState } from "react";
+
+const body = document.querySelector("body");
 
 function App() {
+  const [showMore, setShowMore] = useState(false);
+
+  const handleClick = () => {
+    // const mainSection = body?.querySelector(".main_section");
+    // mainSection?.classList.add("show_more");
+    setShowMore(true);
+    console.log(showMore);
+  };
+
   return (
     <div className="App">
       <div className="backgroundImage"></div>
@@ -30,30 +42,35 @@ function App() {
           </div>
 
           <p id="location">IN LONDON, UK</p>
-          <button>
+          <button onClick={handleClick}>
             more
             <img src={arrowUp} alt="" />
           </button>
         </div>
       </section>
-      <div className="details">
-        <div>
-          <p>CURRENT TIMEZONE</p>
-          <h3>Europe/London</h3>
+
+      {showMore ? (
+        <div className="details">
+          <div>
+            <p>CURRENT TIMEZONE</p>
+            <h3>Europe/London</h3>
+          </div>
+          <div>
+            <p>Day of the year</p>
+            <h3>295</h3>
+          </div>
+          <div>
+            <p>Day of the week</p>
+            <h3>5</h3>
+          </div>
+          <div>
+            <p>Week number</p>
+            <h3>42</h3>
+          </div>
         </div>
-        <div>
-          <p>Day of the year</p>
-          <h3>295</h3>
-        </div>
-        <div>
-          <p>Day of the week</p>
-          <h3>5</h3>
-        </div>
-        <div>
-          <p>Week number</p>
-          <h3>42</h3>
-        </div>
-      </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
