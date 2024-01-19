8c7b9fc44ecb9791638046fe7ad6d72bcf78545e
Bald Eagle <tsereteli.t02@gmail.com>
2024-01-19 15:53:32 +0400

    daytime, without details. css is written

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
 import "./App.css";
 import sunIcon from "../assets/desktop/icon-sun.svg";
+import arrowUp from "../assets/desktop/icon-arrow-up.svg";
+import arrowDown from "../assets/desktop/icon-arrow-down.svg";
 
 function App() {
   return (
     <div className="App">
-      <div className="quote">
-        <p>
-          “The science of operations, as derived from mathematics more
-          especially, is a science of itself, and has its own abstract truth and
-          value.”
-        </p>
-        <p>Ada Lovelace</p>
-      </div>
-      <div className="time">
-        <img src={sunIcon} alt="" />
-        <p>GOOD MORNING</p>
-        <h1>11:37</h1>
-        <span>BST</span>
-        <p>IN LONDON, UK</p>
-        <button>button</button>
-      </div>
+      <div className="backgroundImage"></div>
+      <section className="main_section">
+        <div className="quote">
+          <p>
+            “The science of operations, as derived from mathematics more
+            especially, is a science of itself, and has its own abstract truth
+            and value.”
+          </p>
+          <p id="quote_author">Ada Lovelace</p>
+        </div>
+        <div className="time">
+          <div id="greetings">
+            <img src={sunIcon} alt="" />
+            <p>GOOD MORNING</p>
+          </div>
+          <div id="local_time">
+            <h1>11:37</h1>
+            <span>BST</span>
+          </div>
+
+          <p id="location">IN LONDON, UK</p>
+          <button>
+            more
+            <img src={arrowUp} alt="" />
+          </button>
+        </div>
+      </section>
       <div className="details">
         <div>
           <p>CURRENT TIMEZONE</p>
