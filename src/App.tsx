d7ab2e9d9086832913e4b547baae2581968fd575
Bald Eagle <tsereteli.t02@gmail.com>
2024-01-21 17:10:55 +0400

    idk where

--- src/App.tsx
+++ src/App.tsx
@@ -15,59 +15,52 @@ function App() {
 
   return (
     <div className="App">
-      <section className="main_section">
-        <div className="quote">
-          <div>
-            <p>
-              “The science of operations, as derived from mathematics more
-              especially, is a science of itself, and has its own abstract truth
-              and value.”
-            </p>
-            <p id="quote_author">Ada Lovelace</p>
+      <section className={showMore ? "main_section moveup" : "main_section"}>
+        <div className="moving">
+          <div className={showMore ? "quote hide" : "quote"}>
+            <div>
+              <p>
+                “The science of operations, as derived from mathematics more
+                especially, is a science of itself, and has its own abstract
+                truth and value.”
+              </p>
+              <p id="quote_author">Ada Lovelace</p>
+            </div>
+            <img id="refresh" src={iconRefresh} alt="" />
           </div>
-          <img id="refresh" src={iconRefresh} alt="" />
-        </div>
-        <div className="time">
-          <div id="greetings">
-            <img src={sunIcon} alt="" />
-            <p>GOOD MORNING</p>
-          </div>
-          <div id="local_time">
-            <h1>11:37</h1>
-            <span>BST</span>
-          </div>
+          <div className={showMore ? "time upwards" : "time"}>
+            <div id="greetings">
+              <img src={sunIcon} alt="" />
+              <p>GOOD MORNING</p>
+            </div>
+            <div id="local_time">
+              <h1>11:37</h1>
+              <span>BST</span>
+            </div>
 
-          <p id="location">IN LONDON, UK</p>
-          <button onClick={handleClick}>
-            {showMore ? "less" : "more"}
-            <img src={arrowUp} alt="" />
-          </button>
+            <p id="location">IN LONDON, UK</p>
+            <button onClick={handleClick}>
+              {showMore ? "less" : "more"}
+              <img src={arrowUp} alt="" />
+            </button>
+          </div>
+          <div className="darken"></div>
+          <div className={showMore ? "details show-up" : "details"}>
+            <div className="column">
+              <p className="keys">CURRENT TIMEZONE</p>
+              <p className="keys">Day of the week</p>
+              <p className="keys">Day of the year</p>
+              <p className="keys">Week number</p>
+            </div>
+            <div className="column">
+              <h2 className="values">Europe/London</h2>
+              <h2 className="values">295</h2>
+              <h2 className="values">5</h2>
+              <h2 className="values">42</h2>
+            </div>
+          </div>
         </div>
-        {/* <div className="darken"></div> */}
       </section>
-
-      {showMore ? (
-        <div className="details">
-          <div>
-            <p>CURRENT TIMEZONE</p>
-            <h3>Europe/London</h3>
-          </div>
-          <div>
-            <p>Day of the year</p>
-            <h3>295</h3>
-          </div>
-          <div>
-            <p>Day of the week</p>
-            <h3>5</h3>
-          </div>
-          <div>
-            <p>Week number</p>
-            <h3>42</h3>
-          </div>
-        </div>
-      ) : (
-        <></>
-      )}
     </div>
   );
 }
