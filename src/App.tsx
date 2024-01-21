025ebdd81c772f35e38cf401471952bd72d8c212
Bald Eagle <tsereteli.t02@gmail.com>
2024-01-21 19:36:25 +0400

    added two api fetch

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,64 @@ import "./App.css";
 import sunIcon from "../assets/desktop/icon-sun.svg";
 import arrowUp from "../assets/desktop/icon-arrow-up.svg";
 import iconRefresh from "../assets/desktop/icon-refresh.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const URL = "http://worldtimeapi.org/api/timezone/";
+const timezone = "Asia/Tbilisi";
 
 function App() {
   const [showMore, setShowMore] = useState(false);
+  const [currentTimezone, setCurrentTimezone] = useState<string>("");
+  const [dayOfTheWeek, setDayOfTheWeek] = useState<number>();
+  const [dayOfTheYear, setDayOfTheYear] = useState<number>();
+  const [weekNumber, setWeekNumber] = useState<number>();
+  const [hour, setHour] = useState<number>();
+  const [minutes, setMinutes] = useState<number>();
+  const [abbr, setAbbr] = useState<string>();
+  const [city, setCity] = useState<string>();
+  // const [seconds, setSeconds] = useState<number>();
+  // seconds and secSeconds -> if we need the exact clock in milliseconds accuracy, but it will me more costly for processor <<---
 
   const handleClick = () => {
     setShowMore(!showMore);
     console.log(showMore);
     console.log("i am clicked");
   };
+
+  const fetchData = async () => {
+    const response = await fetch(URL + timezone);
+    const data = await response.json();
+
+    setCurrentTimezone(data.timezone);
+    setDayOfTheWeek(data.day_of_week);
+    setDayOfTheYear(data.day_of_year);
+    setWeekNumber(data.week_number);
+
+    const datetime = new Date(data.datetime);
+    setHour(datetime.getHours());
+    setMinutes(datetime.getMinutes());
+    // setSeconds(datetime.getSeconds()); as said in upper line
+
+    // console.log(data);
+  };
+
+  const country = async () => {
+    const res = await fetch(
+      "https://api.ipbase.com/v2/info?apikey=ipb_live_qmd0BGBMgcbhSXSHy4DDOKPafeorSGm9wfpPjOea&ip=212.58.103.131"
+    );
+    const data = await res.json();
+    setAbbr(data.data.location.country.alpha2);
+    setCity(data.data.location.city.name);
+    console.log(data.data.location.city.name);
+  };
+  useEffect(() => {
+    fetchData();
+    country();
+
+    const intervalId = setInterval(fetchData, 60000);
+
+    return () => clearInterval(intervalId);
+  }, []);
 
   return (
     <div className="App">
@@ -33,11 +81,15 @@ function App() {
             <p>GOOD MORNING</p>
           </div>
           <div id="local_time">
-            <h1>11:37</h1>
+            <h1>
+              {hour! - 12}:{String(minutes).padStart(2, "0")}
+            </h1>
             <span>BST</span>
           </div>
 
-          <p id="location">IN LONDON, UK</p>
+          <p id="location">
+            IN {city}, {abbr}
+          </p>
           <button onClick={handleClick}>
             {showMore ? "less" : "more"}
             <img src={arrowUp} alt="" />
@@ -52,10 +104,10 @@ function App() {
             <p className="keys">Week number</p>
           </div>
           <div className="column">
-            <h2 className="values">Europe/London</h2>
-            <h2 className="values">295</h2>
-            <h2 className="values">5</h2>
-            <h2 className="values">42</h2>
+            <h2 className="values">{currentTimezone}</h2>
+            <h2 className="values">{dayOfTheWeek}</h2>
+            <h2 className="values">{dayOfTheYear}</h2>
+            <h2 className="values">{weekNumber}</h2>
           </div>
         </div>
       </section>
